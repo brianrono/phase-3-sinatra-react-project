fb45daeb29700670860c4ceeac6d3477926656be
brianrono <brianrono16@gmail.com>
2023-06-07 00:12:34 +0300

    Added controllers to work on

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,24 @@
 class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+    
+  set :default_content_type, "application/json"
+
+  # allow access-control-allow-origin header on all requests
+  before do
+      response.headers["Access-Control-Allow-Origin"] = "*"
   end
 
+  # enable CORS preflight requests
+  options "*" do
+      response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+
+      # response.headers("Access-Control-Allow-Origin", "*");
+      # response.headers("Access-Control-Allow-Credentials", "true");
+      # response.headers("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+      # response.headers("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  end
+
+  get '/' do # this is the root route of the application (the homepage) but you can have as many routes as you want
+      {ToDO: "Get backend up and running"}.to_json
+  end
+  
 end
